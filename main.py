94c7911912f9d692c55ec6fa41021c85c4a2e5f5
beNiceFam <133543297+beNiceFam@users.noreply.github.com>
2023-10-14 14:04:00 +0000

    Update main.py
    Changed second-related variable name once more. Added other measures to resemble the site I was used to utilize.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from datetime import date, timedelta
+from datetime import date
+import math
 
 d1, m1, y1 = input("Enter the first date as DD MM YY: ").split()
 d2, m2, y2 = input("And the second one too as DD MM YY: ").split()
@@ -6,10 +7,27 @@ d2, m2, y2 = input("And the second one too as DD MM YY: ").split()
 firstDate = date(int(y1), int(m1), int(d1))
 secondDate = date(int(y2), int(m2), int(d2))
 
-seconds = abs((firstDate - secondDate).total_seconds())
+totalSecs = abs((firstDate - secondDate).total_seconds())
 
-print(str(seconds) + " seconds")
-print(str(seconds/60) + " minutes")
-print(str(seconds/3600) + " hours")
-print(str(seconds/86400) + " days")
-print(str(seconds/604800) + " weeks")
+print()
+print(str(round(totalSecs)) + " seconds")
+print(str(round(totalSecs/60)) + " minutes")
+print(str(round(totalSecs/3600)) + " hours")
+print(str(round(totalSecs/86400)) + " day(s)")
+
+weeks = totalSecs/604800
+daysRest = math.floor(((weeks % 1) * 7))
+
+if weeks % 7 > 0 and weeks >= 1: 
+    print(str(round(weeks)) + " week(s) and " + str(daysRest) + " day(s)")
+elif weeks >= 1:
+    print(str(round(weeks)) + " week(s)")
+
+months = totalSecs/2592000
+daysRest = math.floor(((months % 1) * 30))
+
+if months % 30 > 0 and months >= 1:
+    print(str(round(months)) + " month(s) and " + str(daysRest) + " day(s)")
+elif months >= 1:
+    print(str(round(months)) + " month(s)")
+ 
