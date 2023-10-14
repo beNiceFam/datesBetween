d30157a6c3a3bcb3d45ba2cb6f5174dbecfa2240
beNiceFam <133543297+beNiceFam@users.noreply.github.com>
2023-10-14 14:10:40 +0000

    Update main.py
    Removed a library (math) and shifted functions from floor to round, since it was not nedded.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from datetime import date
-import math
 
 d1, m1, y1 = input("Enter the first date as DD MM YY: ").split()
 d2, m2, y2 = input("And the second one too as DD MM YY: ").split()
@@ -16,7 +15,7 @@ print(str(round(totalSecs/3600)) + " hours")
 print(str(round(totalSecs/86400)) + " day(s)")
 
 weeks = totalSecs/604800
-daysRest = math.floor(((weeks % 1) * 7))
+daysRest = round(((weeks % 1) * 7))
 
 if weeks % 7 > 0 and weeks >= 1: 
     print(str(round(weeks)) + " week(s) and " + str(daysRest) + " day(s)")
@@ -24,10 +23,9 @@ elif weeks >= 1:
     print(str(round(weeks)) + " week(s)")
 
 months = totalSecs/2592000
-daysRest = math.floor(((months % 1) * 30))
+daysRest = round(((months % 1) * 30))
 
 if months % 30 > 0 and months >= 1:
     print(str(round(months)) + " month(s) and " + str(daysRest) + " day(s)")
 elif months >= 1:
     print(str(round(months)) + " month(s)")
- 
